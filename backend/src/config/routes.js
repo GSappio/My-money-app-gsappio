dffcd4539d468e7e780412dfdfe770a17a9f9dc2
Gabriel <gabriel.sappio02@gmail.com>
2022-11-22 10:33:40 -0300

    Route Mapping

---
+++ backend/src/config/routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+module.exports = function(server) {
+    
+    // Definir URL base para todas as rotas
+    const router = express.Router()
+    server.use('/api', router)
+
+   // Rotaas de Ciclo de Pagamentos  
+   const BillingCycle = require('../api/billingCycle/billingCycleService')
+   BillingCycle.register(router, '/billingCycles')
+   
+
+}
