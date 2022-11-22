ef1d8edf216085caddfac6d70e2a5fee8fdc7666
Gabriel <gabriel.sappio02@gmail.com>
2022-11-22 11:24:49 -0300

    Getting the number of records using Count

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -4,21 +4,23 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
 BillingCycle.route('get', (req, res, next) => {
-
     BillingCycle.find({}, (err, docs) => {
-
         if(!err) {
-
             res.json(docs)
-
         } else {
-
-            res.status(500).json({errors: [error]})
-
+            res.status(500).json({errors: [err]})
         }
-
     })
+})
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+    })
 })
 
 
